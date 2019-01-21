800a522d31c919be84159ba6e5750a71ef01bbec
ki7mt <ki7mt@yahoo.com>
2019-01-21 11:05:38 -0700

    Restructure the pgsql folder

--- src/pgsql/utils.sql
+++ src/pgsql/utils.sql
@@ -1,12 +1,12 @@
--- Project .............: R-DaaS
+-- Project .............: ARDS Tools
 -- Author ..............: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
 -- Copyright ...........: Copyright (C) 2018 Greg Beam, KI7MT
 -- License .............: GPL-3
 
--- File ................: R-DaaS_Utility-Views.sql
+-- File ................: utils.sql
 -- Description .........: Database Utility Views
 -- Database Type .......: PostgreSQL v10 or later
--- Version .............: 1.0.0
+-- Version .............: 0.0.1
 -- ADIF Specification ..: 3.0.8
 
 --******************************************************************************
@@ -18,8 +18,8 @@
 \echo 'Creating User Views'
 \echo '-----------------------------'
 
--- View: rdaas.vw_view_list
-CREATE OR REPLACE VIEW rdaas.view_list AS
+-- View: utils.vw_view_list
+CREATE OR REPLACE VIEW utils.view_list AS
     SELECT
         schemaname AS "Schema",
         viewname AS "View Name"
@@ -27,13 +27,13 @@ CREATE OR REPLACE VIEW rdaas.view_list AS
         WHERE schemaname NOT IN ('pg_catalog', 'information_schema')
     ORDER BY schemaname, viewname;
 
--- View: rdaas.vw_db_size_detail
-CREATE OR REPLACE VIEW rdaas.db_size_view AS
+-- View: utils.vw_db_size_detail
+CREATE OR REPLACE VIEW utils.db_size_view AS
     SELECT
         pg_size_pretty(pg_database_size('postgres')) AS "DB Size";
 
--- View: rdaas.vw_db_size_detail
-CREATE OR REPLACE VIEW rdaas.schema_size_view AS
+-- View: utils.vw_db_size_detail
+CREATE OR REPLACE VIEW utils.schema_size_view AS
     SELECT schema_name, 
         pg_size_pretty(sum(table_size)::BIGINT),
         trunc((sum(table_size) / pg_database_size(current_database())) * 100, 2) AS percent
@@ -53,6 +53,6 @@ CREATE OR REPLACE VIEW rdaas.schema_size_view AS
 \echo 'Report Bugs to..: https://github.com/KI7MT/dotnet-core-examples/issues'
 \echo ''
 \echo 'The following Views Are Available For Use'
-select * from rdaas.view_list;
+select * from utils.view_list;
 
--- END rdaas-utility-views.sql
+-- END utils-utility-views.sql
