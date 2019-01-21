ec6316b7d1022628d75e4784fa70a9ff43b3b82e
ki7mt <ki7mt@yahoo.com>
2019-01-21 15:36:16 -0700

    Add toplevel info.sql file

---
+++ info.sql
@@ -0,0 +1,13 @@
+/* 
+
+    Project .............: Amateur Radio Data Serivce Tools
+    Author ..............: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
+    Copyright ...........: Copyright (C) 2018 Greg Beam, KI7MT
+    License .............: GPL-3
+
+    Version .............: 0.0.1
+    ADIF Specification ..: 3.0.9
+    ADIF Refrence .......: http://www.adif.org/309/ADIF_309.htm#Enumerations
+    Bug Reports .........: https://github.com/KI7MT/ards-tools/issues'
+    
+*/
