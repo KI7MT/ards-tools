2bef914f5ecf44c0c4ccf8c693ff4fcc3577b2c4
ki7mt <ki7mt@yahoo.com>
2019-03-03 04:00:13 -0700

    Add Eureka and Zuul/Ribbon projects

--- src/java/ards-adif-api/src/main/java/com/ards/adif/api/ArdsAdifApi.java
+++ src/java/ards-adif-api/src/main/java/com/ards/adif/api/ArdsAdifApi.java
@@ -4,20 +4,22 @@ import org.apache.catalina.servlets.WebdavServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ArdsAdifApi {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ArdsAdifApi.class, args);
 	}
-	
+
 	@Bean
 	public ServletRegistrationBean h2servletRegistration() {
-	    ServletRegistrationBean registration = new ServletRegistrationBean(new WebdavServlet());
-	    registration.addUrlMappings("/console/*");
-	    return registration;
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebdavServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
 	}
-
+	
 }
---
+++ src/java/ards-gateway/src/main/java/com/ards/gateway/ArdsGatewayApplication.java
@@ -0,0 +1,24 @@
+package com.ards.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ArdsGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArdsGatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
+
+}
