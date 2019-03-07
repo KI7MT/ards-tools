c5846b3bc5f60c438997b3719f3e02e80541478c
ki7mt <ki7mt@yahoo.com>
2019-03-07 02:45:24 -0700

    Add test files for consolidating ADIF Tables and data

---
+++ src/pgsql/temp/new-adif-pas-schema.sql
@@ -0,0 +1,67 @@
+-- -----------------------------------------------------------------------------
+-- Primary Administrative Subdivision
+-- -----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS adif.pas;
+
+CREATE TABLE adif.pas
+(
+    pas_id SERIAL PRIMARY KEY,
+    dxcc_id INT NOT NULL,                           -- DXCC Country
+    pas_code CHAR(4) NOT NULL,                      -- code for the subdivision
+    subdivision VARCHAR(120) NOT NULL,              -- subdivision name
+    oblast VARCHAR(90),                             -- Oblast number
+    referred_to_as VARCHAR(60),                     -- Alias Names
+    is_deleted BOOLEAN DEFAULT '0',        -- is a deleted subdivision
+    is_import_only BOOLEAN DEFAULT '0',    -- Can only be imported
+    before_date DATE,                               -- For QSO's Made BEFORE date
+    after_date DATE,                                -- For QSO's made ON or AFTER date
+    CONSTRAINT pas_uq UNIQUE (dxcc_id,pas_code,subdivision)
+);
+
+
+
+-- PAS CQ Zone
+CREATE TABLE adif.pas_code
+(
+    pas_code_id SERIAL PRIMARY KEY,
+    code CHAR(4) NOT NULL,
+    CONSTRAINT pas_code_uq UNIQUE (code)
+);
+
+-- PAS Region
+CREATE TABLE adif.pas_region
+(
+    pas_region_id SERIAL PRIMARY KEY,
+    dxcc_id INT NOT NULL,
+    region VARCHAR(120) NOT NULL,
+    CONSTRAINT pas206_region_uq UNIQUE (region)
+);
+-- FK to dxcc table 
+
+-- PAS CQ Zone
+CREATE TABLE adif.pas_cqzone
+(
+    pas_cqzone_id SERIAL PRIMARY KEY,
+    pas_id INT NOT NULL,
+    cqzone_id INT NOT NULL
+);
+
+-- PAS ITU Zone 
+CREATE TABLE adif.pas_ituzone
+(
+    pas_ituzone_id SERIAL PRIMARY KEY,
+    pas_id INT NOT NULL,
+    ituzone_id INT NOT NULL
+);
+
+-- -----------------------------------------------------------------------------
+-- PAS SUMMARY Tables
+-- -----------------------------------------------------------------------------
+
+-- PAS Type
+CREATE TABLE adif.pas_type
+(
+    pas_type_id SERIAL PRIMARY KEY,
+    pas_type VARCHAR(120) NOT NULL,
+);
